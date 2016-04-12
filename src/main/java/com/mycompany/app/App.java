a1372feb10400cf2e3f877b29c1b63d28fc5fb1f
BooookStore <booookstore@icloud.com>
2016-04-12 11:53:32 +0900

    [Add] Update.

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,46 +1,17 @@
 package com.mycompany.app;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-/**
- * Hello world!
- *
- */
-public class App extends Application
-{
-	/**
-	 *アプリケーションのスタートポイント
-	 *
-	 */
-	public static void main( String[] args )
-	{
+public class App extends Application {
+
+	public static void main(String[] args){
 		launch(args);
 	}
-
-	/**
-	 * javaFXスタートポイント
-	 *
-	 */
+	
 	@Override
-	public void start(Stage primaryStage){
-		primaryStage.setTitle("Hello,World");
-		Button btn = new Button();
-		btn.setText("Say,Hello World");
-		btn.setOnAction(e -> {
-			System.out.println("Hello World");
-		});
-
-		StackPane root = new StackPane();
-		root.getChildren().add(btn);
-
-		primaryStage.setScene(new Scene(root, 300, 250));
+	public void start(Stage primaryStage) throws Exception {
 		primaryStage.show();
 	}
-
+	
 }
