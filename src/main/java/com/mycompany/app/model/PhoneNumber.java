5984064c61ed97c4129bd71deb12a7f98954bc47
BooookStore <booookstore@icloud.com>
2016-04-13 15:50:27 +0900

    [Add] PhoneNumber model change to StringProperty.

--- src/main/java/com/mycompany/app/model/PhoneNumber.java
+++ src/main/java/com/mycompany/app/model/PhoneNumber.java
@@ -1,25 +1,37 @@
 package com.mycompany.app.model;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 /**
  * 電話番号を表します。
  * @author honyaryousuke
  */
 public class PhoneNumber {
 
-    private String _phoneNumber;
+    private StringProperty _phoneNumber = new SimpleStringProperty();
 
+    /**
+     * コンストラクタ
+     * 
+     * @param _phoneNumber 
+     */
     public PhoneNumber(String _phoneNumber) {
-        this._phoneNumber = _phoneNumber;
+        this._phoneNumber.set(_phoneNumber);
     }
 
     public String getPhoneNumber() {
-        return _phoneNumber;
+        return _phoneNumber.get();
     }
 
     public void setPhoneNumber(String _phoneNumber) {
-        this._phoneNumber = _phoneNumber;
+        this._phoneNumber.set(_phoneNumber);
     }
 
+    public StringProperty getPhoneNumberProperty(){
+        return _phoneNumber;
+    }
+    
     @Override
     public String toString() {
         return "PhoneNumber{" + "_phoneNumber=" + _phoneNumber + '}';
